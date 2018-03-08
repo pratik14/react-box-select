986ce586a213bec66e2b81bf3c1a13a12fe0ea1e
pratik.s <pratik.s@go-jek.com>
2018-03-08 23:47:58 +0530

    Add navigation bar and boostrap plugin

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,5 +1,6 @@
 import React, {PureComponent} from 'react';
 import MultiSelect from '../lib';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 class App extends PureComponent {
@@ -15,10 +16,36 @@ class App extends PureComponent {
 
     return(
       <div>
-        <MultiSelect
-          selectedOptions={selectedOptions}
-          selectableOptions={selectableOptions}
-       />
+        <nav className="navbar navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="" />
+          </a>
+        </nav>
+        <div className="container-fluid text-center">
+          <div className="row content">
+            <div className="col-sm-2 sidenav">
+              <p><a href="#">Link</a></p>
+              <p><a href="#">Link</a></p>
+              <p><a href="#">Link</a></p>
+            </div>
+            <div className="col-sm-8 text-left">
+            <MultiSelect
+              selectedOptions={selectedOptions}
+              selectableOptions={selectableOptions}
+           />
+            </div>
+            <div className="col-sm-2 sidenav">
+              <div className="well">
+                <p>ADS</p>
+              </div>
+              <div className="well">
+                <p>ADS</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
       </div>
     )
   }
