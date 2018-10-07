1f69c4a7d9921233786f3db920ac016f85022878
Pratik Shah <pratik14shah@gmail.com>
2018-10-07 12:55:25 +0530

    Demo page updated

--- src/demo/App.js
+++ src/demo/App.js
@@ -1,54 +1,142 @@
-import React, {PureComponent} from 'react';
-import MultiSelect from '../lib';
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import React, {PureComponent} from "react";
+import MultiSelect from "../lib";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends PureComponent {
-  render(){
-    var step, options = [];
-    for (step = 1; step < 100; step++) {
-      options.push({id: step, label: 'Element ' + step})
+  options() {
+    var step,
+      options = [];
+    for (step = 1; step < 10; step++) {
+      options.push({id: step, label: "Element " + step});
     }
+    return options;
+  }
 
-    return(
+  renderGettingStarted() {
+    return (
+      <div>
+        <h3>Getting Started</h3>
+
+        <p>
+          Start by installing react-select
+          <pre>{`yarn add react-select`}</pre>
+        </p>
+
+        <p>
+          Import the default export and render in your component:
+          <pre>
+            {`
+import React, { Component } from 'react';
+import Select from 'react-select'
+
+var step, options = [];
+for (step = 1; step < 100; step++) {
+  options.push({id: step, label: "Element " + step});
+}
+
+<MultiSelect options={options} />
+`}
+          </pre>
+        </p>
+      </div>
+    );
+  }
+
+  renderProps() {
+    return (
+      <div>
+        <h3> Props </h3>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Type</th>
+              <th>Default</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>afterSelect</td>
+              <td>function</td>
+              <td>
+                <code>
+                  function()
+                  {}
+                </code>
+              </td>
+              <td>Function to call after one item is selected.</td>
+            </tr>
+            <tr>
+              <td>afterDeselect</td>
+              <td>function</td>
+              <td>
+                <code>
+                  function()
+                  {}
+                </code>
+              </td>
+              <td>Function to call after one item is deselected.</td>
+            </tr>
+            <tr>
+              <td>showSelectAllBtn</td>
+              <td>bool</td>
+              <td>
+                <code>false</code>
+              </td>
+              <td>Show/Hide select/deselect all button.</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  renderDemos() {
+    return (
+      <div>
+        <h3>Demos</h3>
+
+        <b>Select/Unselect All Button</b>
+        <MultiSelect options={this.options()} showSelectAllBtn={true} />
+
+        <b>Callback</b>
+        <MultiSelect
+          options={this.options()}
+          onSelect={val => {
+            alert("Selected label:" + val.label);
+          }}
+          onDeSelect={val => {
+            alert("Deselected label:" + val.label);
+          }}
+        />
+      </div>
+    );
+  }
+
+  render() {
+    return (
       <div>
         <nav className="navbar navbar-light bg-light">
-          <a className="navbar-brand" href="#">
-            <img src="/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="" />
-          </a>
+          <h2>React Multiselect</h2>
         </nav>
+
         <div className="container-fluid text-center">
-          <div className="row content">
-            <div className="col-sm-2 sidenav">
-              <p><a href="#">Link</a></p>
-              <p><a href="#">Link</a></p>
-              <p><a href="#">Link</a></p>
-            </div>
-            <div className="col-sm-2 text-left" />
-            <div className="col-sm-4 text-left">
-              <MultiSelect
-                options={options}
-              />
-              <MultiSelect
-                options={options}
-                showSelectAllBtn={true}
-              />
-            </div>
-            <div className="col-sm-2 text-left" />
-            <div className="col-sm-2 sidenav">
-              <div className="well">
-                <p>ADS</p>
+          <div className="row">
+            <div className="col-md-3" />
+            <div className="col-md-6 text-left">
+              <div className="col-md-12">{this.renderGettingStarted()}</div>
+              <div className="col-md-12">
+                <MultiSelect options={this.options()} />
               </div>
-              <div className="well">
-                <p>ADS</p>
-              </div>
+              <div className="col-md-12">{this.renderProps()}</div>
+              <div className="col-md-12">{this.renderDemos()}</div>
             </div>
+            <div className="col-md-3" />
           </div>
         </div>
-
-
       </div>
-    )
+    );
   }
 }
 
